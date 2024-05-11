a2da8ef485abab62465dc7372e338bee32be7526
Santiago Corredor 17 <sa.an.walteros@unillanos.edu.co>
2024-05-11 16:27:21 -0500

    UNNGRAN PASO PARA TODOOSSS

--- Proyecto Final.cpp
+++ Proyecto Final.cpp
@@ -1,7 +1,7 @@
 #include "iostream"
 #include "string"
 using namespace std;
-// eso pepe
+
 void matriz1();
 
 int main(){
@@ -23,46 +23,80 @@ if(a==1){
 
 matriz1();}}
 
-void matriz1(){
+void matriz1() {
     char matriz[26][26];
-   string alf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    int r;
+    //cin.ignore();
+    string alf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int r, k;
     string palabra;
     string clave;
     string paci;
-    int po=0,longitudPalabra=0,longitudClave=0;
+    string palabracifrada;
+    int po = 0, longitudPalabra = 0, longitudClave = 0;
 
-    cout<<"-----------------------------------------"<<endl;
+    cout << "-----------------------------------------" << endl;
 
-    for(int fila = 0; fila < 26; fila++){
+    for (int fila = 0; fila < 26; fila++) {
         r = fila;
-        for(int columna = 0; columna < 26; columna++) {
+        for (int columna = 0; columna < 26; columna++) {
             matriz[fila][columna] = alf[r % 26];
-            r++;}}
+            r++;
+        }
+    }
 
-    cout << " digite la palabra a cifrar" << endl;
+    cout << " digite la palabra a cifrar sin espacios" << endl;
     cin >> palabra;
-
-    paci=palabra;
+    paci = palabra;
 
     cout << "digite la clave" << endl;
-    cin >>clave;
+    cin >> clave;
 
     longitudPalabra = palabra.length();
     longitudClave = clave.length();
 
-    cout<<"la palabra a cifrar tiene: "<<longitudPalabra<<" caracteres"<<endl;
+    cout << "la palabra a cifrar tiene: " << longitudPalabra << " caracteres" << endl;
 
-    for ( po=0; po<longitudPalabra;po++) {
+//arrastrar la clave conforme la palabra
+    for (po = 0; po < longitudPalabra; po++) {
+        paci[po] = clave[po % longitudClave];
+    }
 
-        paci[po]=clave[po%longitudClave];}
 
-    cout<<"la palabra original es: "<<palabra<<endl;
-    cout<<"la palabra cifrada es: "<<paci<<endl;
-    cout<<"------------es hora de cifrar------------"<<endl;
+    cout << "la palabra original es: " << palabra << endl;
+    cout << "la palabra medio cifrada es: " << paci << endl;
+    cout << "------------es hora de cifrar------------" << endl;
+    cout << "------------matriz de vigenere------------" << endl;
+    for (int fila = 0; fila < 26; fila++) {
+        for (int columna = 0; columna < 26; columna++) {
+            cout << matriz[fila][columna] << "|";
+        }
+        cout << endl;
+    }
+    
+    int posicioncol;
+    int posicionfil;
 
-    for(int fila = 0; fila < 26; fila++){
-        for(int columna = 0; columna < 26; columna++){
-            cout << matriz[fila][columna] << "|";}
-        cout << endl;}}
+    for (int columna = 0; columna < 26; columna++) {
 
+        if (palabra[0] == matriz[0][columna]) {
+            cout << "encontraste " << matriz[0][columna] << "!!!" << endl;
+
+            posicioncol = columna;
+            cout << posicioncol << endl;
+        }
+    }
+    for (int fila = 0; fila < 26; fila++) {
+
+        if (paci[0] == matriz[fila][0]) {
+            cout << "encontraste " << matriz[fila][0] << "!!!" << endl;
+            posicionfil = fila;
+            cout << posicionfil << endl;
+        }
+
+
+    }
+    //imprimir caracter interseccion
+    cout << matriz[posicionfil][posicioncol];
+
+
+}
